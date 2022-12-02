7dd6bf27737e439e9db1b23ce2383391b9f1e609
JCCH1 <jmachuca20@alumnos.utalca.cl>
2022-12-02 16:12:59 -0300

    T001-H002: crear un menú de selección para iniciar y reiniciar el juego
    tarea completada tarde 10 min (creado en consola)

---
+++ juanmain.py
@@ -0,0 +1,13 @@
+print("bienvenido al juego buscaCovid")
+print("seleccione si quiere jugar o reiniciar el juego")
+print("Iniciar juego")
+i=0
+while i<10:
+  x=int(input("pulse [1] para iniciar pulse [2] para reiniciar \n"))
+  if (x==1):
+    print("seleccionó iniciar partida")
+    i=12
+  if (x==2):
+    print("Selecionó reiniciar partida")
+    i=12
+    
