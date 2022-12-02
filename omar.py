0ce9d53e62fc9dec0f05c6fcd67ce91999244616
Krx-toska <102070663+Krx-toska@users.noreply.github.com>
2022-12-02 16:50:53 -0300

    hu1

---
+++ omar.py
@@ -0,0 +1,28 @@
+def crearmatriz(tamaño):
+  matriz = []
+  for i in range(tamaño):
+    matriz.append([])
+    for j in range(tamaño):
+        matriz[i].append(0)
+  return matriz
+
+
+print("   menu de dificultad   ")
+print("1     principiante    ")
+print("2     intermedio   ")
+print("3      avanzado   ")
+opc = int(input())
+matriz=[]
+opc 2 = 
+if(opc==1):
+  print("principiante")
+  matriz=crearmatriz(9)
+  
+elif(opc==2):
+  print("intermedio")
+  matriz=crearmatriz(16)
+elif(opc==3):
+  print("avanzado")
+  matriz=crearmatriz(32)
+
+print(matriz)
